d935f1efa2035a842fb44914889c258e9e26d649
Mard4sh <167506550+Mard4sh@users.noreply.github.com>
2024-04-27 01:29:16 +0500

    Добавление main метода и графического оформления

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,79 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Класс Main
 public class Main
 {
     public static void main(String[] args)
-        {
-        System.out.println("Hello world!");
-        }
+    {
+        new CalculatorGUI();
+    }
+}
+
+// Главное окно
+class CalculatorGUI extends JFrame implements ActionListener 
+{
+    private JRadioButton[] foundationButtons, wallButtons, roofButtons, finishingButtons;
+    private JTextField areaField;
+    private JTextArea resultArea;
+    private JButton calculateButton;
+
+    public CalculatorGUI() 
+    {
+        super("Калькулятор стоимости строительства частного дома");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 510);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        // Фундамент
+        JPanel foundationPanel = createElementPanel("Фундамент",
+                new String[]{"Ленточный (4000 руб)", "Плитный (5000 руб)", "Свайный (3000 руб)"},
+                new double[]{4000, 5000, 3000});
+        add(foundationPanel);
+
+        // Стены
+        JPanel wallButtons = createElementPanel("Стены",
+                new String[]{"Кирпичные (8000 руб)", "Газобетонные (5000 руб)", "Каркасные (3000 руб)"},
+                new double[]{8000, 6000, 4000});
+        add(wallButtons);
+
+        // Крыша
+        JPanel roofButtons = createElementPanel("Крыша",
+                new String[]{"Металлочерепица (4000 руб)", "Мягкая кровля (3000 руб)", "Шифер (2000 руб)"},
+                new double[]{4000, 3000, 2000});
+        add(roofButtons);
+
+        // Отделка
+        JPanel finishingButtons = createElementPanel("Отделка",
+                new String[]{"Ленточный (8000 руб)", "Плитный (5000 руб)", "Свайный (3000 руб)"},
+                new double[]{8000, 5000, 3000});
+        add(finishingButtons);
+
+        // Площадь
+        areaField = new JTextField(10);
+        JPanel areaPanel = new JPanel();
+        areaPanel.add(new JLabel("Площадь дома (кв.м.):"));
+        areaPanel.add(areaField);
+        add(areaPanel);
+
+        
+        // Кнопка
+        JPanel buttonPanel = new JPanel();
+        calculateButton = new JButton("Рассчитать");
+        calculateButton.addActionListener(this);
+        buttonPanel.add(calculateButton);
+        add(buttonPanel);
+        
+        // Область вывода
+        JPanel resultPanel = new JPanel();
+        resultPanel.add(new JScrollPane(resultArea));
+        resultArea = new JTextArea();
+        resultArea.setEditable(false);
+        add(new JScrollPane(resultArea));
+        add(resultPanel);
+
+        setVisible(true);
+    }
 }
